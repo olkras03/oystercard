786082acdd21c81109cfd425bdc89af87fbf49bb
Lena Krasnova <olkras03@gmail.com>
2019-10-21 14:02:39 +0100

    next failing test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,11 @@
+require 'oystercard'
+
 describe Oystercard do
-  it "should be an instance of Oystercard" do
+  it 'should be an instance of Oystercard' do
     expect(subject).to be_instance_of Oystercard
+  end
+  
+  it 'should respond to balance' do
+    expect(subject).to respond_to(:balance)
   end
 end
