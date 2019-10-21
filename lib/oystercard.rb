43bd9a502d2ca4d00535f40cc1d0fdb366c2dbfe
Lena Krasnova <olkras03@gmail.com>
2019-10-21 15:07:40 +0100

    test passed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -11,4 +11,8 @@ class Oystercard
     raise "Cannot top up : #{LIMIT} reached" if (@balance + amount) > LIMIT
     @balance += amount
   end
+
+  def deduct(amount)
+    @balance -= amount
+  end
 end
