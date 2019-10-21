13df0ce24e5710da1be34e20ccfae67298538863
Lena Krasnova <olkras03@gmail.com>
2019-10-21 14:37:47 +0100

    failing test for enforcing max limit

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,16 +7,20 @@ describe Oystercard do
     expect(oystercard).to be_instance_of Oystercard
   end
 
-  it "should respond to balance" do
+  it "responds to balance" do
     expect(oystercard).to respond_to(:balance)
   end
 
-  it "should show initial balance as 0" do
+  it "shows initial balance as 0" do
     expect(oystercard.balance).to eq(0)
   end
 
-  it "should top up balance" do
+  it "tops up balance" do
     oystercard.top_up(10)
     expect(oystercard.balance).to eq(10)
+  end
+
+  it "raises an error if top up more then 90" do
+    expect { oystercard.top_up(91) }.to raise_error
   end
 end
